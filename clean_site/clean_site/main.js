3234f482a351f5e43699884a04c2165ffefee8ec
bambooneko <142969349+bambooneko@users.noreply.github.com>
2026-01-05 00:12:31 +0800

    Add files via upload

---
+++ clean_site/clean_site/main.js
@@ -0,0 +1,1 @@
+new Swiper('.swiper',{loop:true,autoplay:{delay:4000,disableOnInteraction:false},pagination:{el:'.swiper-pagination',clickable:true},navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev'}});
